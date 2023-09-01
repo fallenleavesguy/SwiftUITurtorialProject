6695d8ef9e8ce49c51f92f6e411fb5c1a4c3dc55
donghs <9261562+fallenleavesguy@users.noreply.github.com>
2023-09-01 23:03:33 +0800

    add navigation

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by donghs on 2023/9/1.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack(spacing: 0.0) {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
